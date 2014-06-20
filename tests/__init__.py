f193fe688792c225dcf521cb35e2bc276dd75d3f
Henning Schroder <henning.schroeder@gmail.com>
2014-06-20 17:45:16 +0200

    applied autopep8 on setup.py and tests, too

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,18 +3,20 @@ if platform.python_version() < '2.7':
     import unittest2 as unittest
 else:
     import unittest
-    
+
+
 class ManualClock(object):
+
     def __init__(self):
         super(ManualClock, self).__init__()
         self.now = 0
-        
+
     def add(self, value):
         self.now = self.now + value
-        
+
     def time(self):
         return self.now
-    
+
+
 class TimedTestCase(unittest.TestCase):
     clock = ManualClock()
-    
--- tests/test__counter.py
+++ tests/test__counter.py
@@ -1,21 +1,24 @@
 from . import TimedTestCase
 from pyformance.meters import Counter
 
+
 class CounterTestCase(TimedTestCase):
-    
+
     def setUp(self):
         super(CounterTestCase, self).setUp()
         self.counter = Counter()
+
     def tearDown(self):
         super(CounterTestCase, self).tearDown()
-        
+
     def test__inc(self):
         before = self.counter.get_count()
         self.counter.inc()
         after = self.counter.get_count()
-        self.assertEqual(before+1,after)
+        self.assertEqual(before + 1, after)
+
     def test__dec(self):
         before = self.counter.get_count()
         self.counter.dec()
         after = self.counter.get_count()
-        self.assertEqual(before-1,after)
+        self.assertEqual(before - 1, after)
