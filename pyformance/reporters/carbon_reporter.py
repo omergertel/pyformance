cac9ed5f131c9f259654b97f00bdf18fffeda71b
Henning Schroder <henning.schroeder@gmail.com>
2014-07-02 18:01:16 +0200

    reporter to send metrics to graphite via carbon-daemon over tcp

---
+++ pyformance/reporters/carbon_reporter.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Carbon is the network daemon to collect metrics for Graphite
+"""
+import time
+import socket
+
+from .reporter import Reporter
+
+DEFAULT_CARBON_SERVER = '0.0.0.0'
+DEFAULT_CARBON_PORT = 2003
+
+
+class CarbonReporter(Reporter):
+    
+    def __init__(self, registry, reporting_interval, prefix="", 
+                 server=DEFAULT_CARBON_SERVER, port=DEFAULT_CARBON_PORT, socket_factory=socket.socket):
+        self.prefix = prefix
+        self.server = server
+        self.port = port
+        self.socket_factory = socket_factory
+        super(CarbonReporter, self).__init__(registry, reporting_interval)
+
+    def send_metrics_now(self, registry=None):
+        metrics = self._collect_metrics(registry or self.registry)
+        if metrics:
+            # XXX: keep connection open or use UDP?
+            sock = self.socket_factory()
+            sock.connect((CARBON_SERVER, CARBON_PORT))
+            sock.sendall(metrics)
+            sock.close()
+            
+    def _collect_metrics(self, registry, timestamp=None):
+        timestamp = timestamp or int(round(time.time()))
+        metrics = registry.dump_metrics()
+        metrics_data = []
+        for key in metrics.keys():
+            for value_key in metrics[key].keys():
+                metricLine = "%s%s.%s %s %s\n" % (self.prefix, key, value_key, metrics[key][value_key], timestamp)
+                metrics_data.append(metricLine)
+        return ''.join(metrics_data)
+
+
