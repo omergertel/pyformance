1642ad966fecf908f5558feb0d7948eebccfe25e
Gregor Zeitlinger <gregor.zeitlinger@minodes.com>
2015-03-26 11:12:46 +0100

    check if thread is already running

--- pyformance/reporters/reporter.py
+++ pyformance/reporters/reporter.py
@@ -5,19 +5,32 @@ from pyformance.registry import global_registry
 
 class Reporter(object):
 
+    def create_thread(self):
+        # noinspection PyAttributeOutsideInit
+        self._loop_thread = Thread(target=self._loop, name="pyformance reporter {}".format(type(self).__qualname__))
+        self._loop_thread.setDaemon(True)
+
     def __init__(self, registry=None, reporting_interval=30, clock=None):
         self.registry = registry or global_registry()
         self.reporting_interval = reporting_interval
         self.clock = clock or time
         self._stopped = Event()
-        self._loop_thread = Thread(target=self._loop, name="pyformance reporter {}".format(type(self).__qualname__))
-        self._loop_thread.setDaemon(True)
+        self.create_thread()
 
     def start(self):
-        if not self._stopped.is_set() and not self._loop_thread._started.is_set():
-            self._loop_thread.start()
-            return True
-        return False
+        if self._stopped.is_set():
+            return False
+
+        r = str(self._loop_thread)
+        if "stopped" in r:
+            # has to be recreated in a celery worker
+            self.create_thread()
+        elif "started" in r:
+            # already started
+            return False
+
+        self._loop_thread.start()
+        return True
 
     def stop(self):
         self._stopped.set()
