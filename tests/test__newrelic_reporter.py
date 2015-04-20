c346e9e90a7e287971b9726833393880fff4af4f
Gregor Zeitlinger <gregor.zeitlinger@minodes.com>
2015-04-20 11:43:54 +0200

    add newrelic reporter

--- tests/test__newrelic_reporter.py
+++ tests/test__newrelic_reporter.py
@@ -8,7 +8,6 @@ from tests import TimedTestCase
 
 
 class TestNewRelicReporter(TimedTestCase):
-
     def setUp(self):
         super(TestNewRelicReporter, self).setUp()
         self.registry = MetricsRegistry(clock=self.clock)
@@ -35,6 +34,13 @@ class TestNewRelicReporter(TimedTestCase):
             c2.dec()
             self.clock.add(1)
         output = r.collect_metrics(self.registry)
-        self.assertEqual('{"agent": {"host": "%s", "pid": %s, "version": "0.3.1"}, "components": [{"duration": 1, "guid": "com.github.pyformance", "metrics": {"Component/c1/count": 1, "Component/counter-2/count": -2, "Component/hist/75_percentile": 160.0, "Component/hist/95_percentile": 512, "Component/hist/999_percentile": 512, "Component/hist/99_percentile": 512, "Component/hist/avg": 102.3, "Component/hist/count": 10.0, "Component/hist/max": 512, "Component/hist/min": 1, "Component/hist/std_dev": 164.94851048466947, "Component/m1/15m_rate": 0, "Component/m1/1m_rate": 0, "Component/m1/5m_rate": 0, "Component/m1/count": 1.0, "Component/m1/mean_rate": 1.0, "Component/t1/15m_rate": 0, "Component/t1/1m_rate": 0, "Component/t1/5m_rate": 0, "Component/t1/75_percentile": 1, "Component/t1/95_percentile": 1, "Component/t1/999_percentile": 1, "Component/t1/99_percentile": 1, "Component/t1/avg": 1.0, "Component/t1/count": 1.0, "Component/t1/max": 1, "Component/t1/mean_rate": 1.0, "Component/t1/min": 1, "Component/t1/std_dev": 0.0, "Component/t1/sum": 1.0}, "name": "%s"}]}'
-                         % (socket.gethostname(), os.getpid(), socket.gethostname()),
-                         output)
+        expected = '{"agent": {"host": "%s", "pid": %s, "version": "0.3.1"}, "components": [{"duration": 1, "guid": "com.github.pyformance", ' \
+                   '"metrics": {"Component/c1/count": 1, "Component/counter-2/count": -2, "Component/hist/75_percentile": 160, "Component/hist/95_percentile": 512, ' \
+                   '"Component/hist/999_percentile": 512, "Component/hist/99_percentile": 512, "Component/hist/avg": 102.3, "Component/hist/count": 10, ' \
+                   '"Component/hist/max": 512, "Component/hist/min": 1, "Component/hist/std_dev": 164.94851048466947, "Component/m1/15m_rate": 0, "Component/m1/1m_rate": 0, ' \
+                   '"Component/m1/5m_rate": 0, "Component/m1/count": 1, "Component/m1/mean_rate": 1, "Component/t1/15m_rate": 0, "Component/t1/1m_rate": 0, ' \
+                   '"Component/t1/5m_rate": 0, "Component/t1/75_percentile": 1, "Component/t1/95_percentile": 1, "Component/t1/999_percentile": 1, ' \
+                   '"Component/t1/99_percentile": 1, "Component/t1/avg": 1, "Component/t1/count": 1, "Component/t1/max": 1, "Component/t1/mean_rate": 1, ' \
+                   '"Component/t1/min": 1, "Component/t1/std_dev": 0, "Component/t1/sum": 1}, "name": "%s"}]}' % (
+                       socket.gethostname(), os.getpid(), socket.gethostname())
+        self.assertEqual(expected.replace(".0", ""), output.replace(".0", ""))
