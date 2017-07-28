8a4ca1b8edd9f72b0f98009edb0c1fd3b57a62a9
spobrdatalab03 <polis@spobrdatalab03.com>
2017-07-28 15:05:01 -0300

    Add JSONReport.py

--- pyformance/reporters/console_reporter.py
+++ pyformance/reporters/console_reporter.py
@@ -19,8 +19,7 @@ class ConsoleReporter(Reporter):
 
     def report_now(self, registry=None, timestamp=None):
         metrics = self._collect_metrics(registry or self.registry, timestamp)
-        for line in metrics:
-            print(line, file=self.stream)
+        print(metrics, file=self.stream)
 
     def _collect_metrics(self, registry, timestamp=None):
         timestamp = timestamp or int(round(self.clock.time()))
