2dae1f879527fce286600c07d0e42577ed7ab134
spobrdatalab03 <polis@spobrdatalab03.com>
2017-07-28 16:17:19 -0300

    FIX create report_now in json_report

--- pyformance/reporters/console_reporter.py
+++ pyformance/reporters/console_reporter.py
@@ -19,7 +19,8 @@ class ConsoleReporter(Reporter):
 
     def report_now(self, registry=None, timestamp=None):
         metrics = self._collect_metrics(registry or self.registry, timestamp)
-        print(metrics, file=self.stream)
+        for line in metrics:
+            print(metrics, file=self.stream)
 
     def _collect_metrics(self, registry, timestamp=None):
         timestamp = timestamp or int(round(self.clock.time()))
